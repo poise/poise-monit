dceb8355cce12d3f7a46b7cc101b3497dce53ab4
Noah Kantrowitz <noah@coderanger.net>
2016-01-04 21:20:08 -0800

    Fix newlines.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -54,9 +54,9 @@ end
 describe 'default' do
   it_should_behave_like 'a monit_test', 'monit', 5000
 end
+
 # Wait at least the default daemon interval.
 Kernel.sleep 180
-
 
 describe 'system provider', unless: File.exist?('/no_system') do
   it_should_behave_like 'a monit_test', 'system', 6000
