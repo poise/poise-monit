a730d1586881325651d4a95e44909d487ea18382
Noah Kantrowitz <noah@coderanger.net>
2016-04-06 14:42:28 -0700

    Fix the CPU spinning from the tests calling sleep(0).

--- test/spec/resources/monit_service_spec.rb
+++ test/spec/resources/monit_service_spec.rb
@@ -33,7 +33,7 @@ describe PoiseMonit::Resources::MonitService do
     # Override these so our tests don't take forever.
     override_attributes['poise-monit'] ||= {}
     override_attributes['poise-monit']['monit_service_timeout'] = 1
-    override_attributes['poise-monit']['monit_service_wait'] = 0
+    override_attributes['poise-monit']['monit_service_wait'] = 0.1
     # Status for simple cases.
     stub_status(status) if status
   end
